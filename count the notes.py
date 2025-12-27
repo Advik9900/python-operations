763263eede762dbc4d2c8beb7fbab4e8bb7aeb52
Advik9900 <advikarora86@gmail.com>
2025-12-27 19:24:36 +0530

    calculating percentage

--- count the notes.py
+++ count the notes.py
@@ -5,4 +5,3 @@ note_3 = ((Amount%100)%50)//10
 print("notes of 100 rupee" ,note_1)
 print("notes of 50 rupee" , note_2)
 print("notes of 10 rupee" , note_3)
-
---
+++ percentage calculation.py
@@ -0,0 +1,8 @@
+print("Enter marks obtained in 4 subject: ")
+maths = int(input("maths : "))
+english = int(input("english : "))
+science = int(input("science : "))
+hindi = int(input("hindi: "))
+sum = maths+english+science+hindi
+prec = (sum/400)*100
+print(prec)
